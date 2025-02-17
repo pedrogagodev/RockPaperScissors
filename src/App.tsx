b6dd80780c99a053c94a76eb2b8c6d898f989eb3
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-17 14:11:15 -0400

    feat: add routes and create Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { useState } from "react";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Option } from "./components/Option";
-import { RulesModal } from "./components/RulesModal";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleModalOpen = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
-
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <RulesModal isModalOpen={isModalOpen} onModalClose={handleModalClose} />
-      <div className="flex-grow flex flex-col items-center mt-24">
-        <h2 className="font-bold text-2xl mb-8">Choose one:</h2>
-        <Option />
-      </div>
-      <Footer onModalOpen={handleModalOpen} />
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
