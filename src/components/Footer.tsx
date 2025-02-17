77c69996354d8000e5c464726bea796e60a0d295
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-16 20:54:04 -0400

    refactor: change text style in footer and add cursor pointer in hover

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,20 @@
 export function Footer() {
   return (
     <div className="bg-[#000B58] text-white flex flex-col items-center py-1 gap-2">
-      <button type="button" className="border p-2 rounded-lg mt-2 bg-amber-50 text-black font-bold">View Rules</button>
+      <button
+        type="button"
+        className="border p-2 rounded-lg mt-2 bg-amber-50 text-black font-bold hover:cursor-pointer"
+      >
+        View Rules
+      </button>
       <h3>
         Developed by{" "}
-        <a href="https://github.com/pedrogagodev">Pedro Oliveira</a>
+        <a
+          href="https://github.com/pedrogagodev"
+          className="hover:cursor-pointer text-amber-100 font-medium"
+        >
+          Pedro Oliveira
+        </a>
       </h3>
     </div>
   );
