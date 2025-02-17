54aa5029ae4fa98ca1657e61bf424e3a85bd60cb
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-17 14:39:10 -0400

    feat: create Result page

---
+++ src/pages/Result.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
+import IconRock from "../assets/images/icon-rock.svg";
+import IconPaper from "../assets/images/icon-paper.svg";
+import IconScissors from "../assets/images/icon-scissors.svg";
+
+export function Result() {
+  const navigate = useNavigate();
+
+  const handleClickTryAgain = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-24 gap-2">
+      <div className="flex gap-12 font-bold text-lg">
+        <h2>Player</h2>
+        <h2>Machine</h2>
+      </div>
+      <div className="flex gap-8">
+        <img src={IconPaper} alt="" />
+        <span>X</span>
+        <img src={IconRock} alt="" />
+      </div>
+      <h2 className="text-xl">Player lose!</h2>
+      <button
+        type="button"
+        className="hover:cursor-pointer bg-[#000B58] text-white rounded-md p-2"
+        onClick={handleClickTryAgain}
+      >
+        Try again!
+      </button>
+    </div>
+  );
+}
