1d32f80a8f25faa11594e142609f6902a7c6c33b
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-18 19:40:50 -0400

    refactor: changes Home.tsx to PlayGame.tsx

--- src/pages/PlayGame.tsx
+++ src/pages/PlayGame.tsx
@@ -1,6 +1,6 @@
 import { Option } from "../components/Option";
 
-export function Home() {
+export function PlayGame() {
   return (
     <div className="flex-grow flex flex-col items-center mt-24">
       <h2 className="font-bold text-2xl mb-8">Choose one:</h2>
