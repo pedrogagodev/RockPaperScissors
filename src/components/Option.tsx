f213b9fae2afc213192f1e20d85d2b71da49dd14
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-24 01:44:02 -0400

    feat: capturing player and machine choice

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,17 +1,33 @@
 import IconRock from "../assets/images/icon-rock.svg";
 import IconPaper from "../assets/images/icon-paper.svg";
 import IconScissors from "../assets/images/icon-scissors.svg";
+import type { GameOption } from "../types/GameOption";
 
-export function Option() {
+type OptionType = {
+  setPlayerChoice: (option: GameOption) => void;
+};
+export function Option({ setPlayerChoice }: OptionType) {
   return (
     <div className="flex gap-10">
-      <button type="button" className="hover:cursor-pointer">
+      <button
+        type="button"
+        className="hover:cursor-pointer"
+        onClick={() => setPlayerChoice("rock")}
+      >
         <img src={IconRock} alt="Rock hand" />
       </button>
-      <button type="button" className="hover:cursor-pointer">
+      <button
+        type="button"
+        className="hover:cursor-pointer"
+        onClick={() => setPlayerChoice("paper")}
+      >
         <img src={IconPaper} alt="Paper hand" />
       </button>
-      <button type="button" className="hover:cursor-pointer">
+      <button
+        type="button"
+        className="hover:cursor-pointer"
+        onClick={() => setPlayerChoice("scissors")}
+      >
         <img src={IconScissors} alt="Scissors hand" />
       </button>
     </div>
--- src/pages/PlayGame.tsx
+++ src/pages/PlayGame.tsx
@@ -1,10 +1,36 @@
 import { Option } from "../components/Option";
-
+import { useGameContext } from "../hooks/useGameContext";
+import type { GameOption } from "../types/GameOption";
 export function PlayGame() {
+  const { dispatch, state } = useGameContext();
+
+  const handlePlayerChoice = (option: GameOption) => {
+    dispatch({
+      type: "SET_PLAYER_CHOICE",
+      option,
+    });
+
+    generateMachineChoice();
+
+  };
+
+  function generateMachineChoice() {
+    const options: GameOption[] = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    const randomMachineChoice = options[randomIndex];
+    dispatch({
+      type: "SET_MACHINE_CHOICE",
+      option: randomMachineChoice,
+    });
+  }
+
+  console.log(
+    `Player Choice: ${state.playerChoice} | Machine Choice: ${state.machineChoice}`
+  );
   return (
     <div className="flex-grow flex flex-col items-center mt-24">
       <h2 className="font-bold text-2xl mb-8">Choose one:</h2>
-      <Option />
+      <Option setPlayerChoice={handlePlayerChoice} />
     </div>
   );
 }
