12e9791a152e89c40d54d6a3b70f1d25b93d262d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-26 00:09:24 -0400

    feat: get a player and machine choice

--- src/pages/PlayGame.tsx
+++ src/pages/PlayGame.tsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { Option } from "../components/Option";
 import { useGameContext } from "../hooks/useGameContext";
 import type { GameOption } from "../types/GameOption";
 export function PlayGame() {
-  const { dispatch, state } = useGameContext();
+  const navigate = useNavigate();
+  const { dispatch } = useGameContext();
 
   const handlePlayerChoice = (option: GameOption) => {
     dispatch({
@@ -12,6 +14,7 @@ export function PlayGame() {
 
     generateMachineChoice();
 
+    navigate("/result");
   };
 
   function generateMachineChoice() {
@@ -23,10 +26,6 @@ export function PlayGame() {
       option: randomMachineChoice,
     });
   }
-
-  console.log(
-    `Player Choice: ${state.playerChoice} | Machine Choice: ${state.machineChoice}`
-  );
   return (
     <div className="flex-grow flex flex-col items-center mt-24">
       <h2 className="font-bold text-2xl mb-8">Choose one:</h2>
